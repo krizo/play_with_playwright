1b8c534496cd07c6f42ef06e5be6c79f215298d8
Krzysztof Bober <krzysztof.bober@inexto.com>
2024-02-08 18:19:08 +0100

    Check negative scenarios

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,18 +1,31 @@
-from playwright.sync_api import Browser, Page
+from playwright.sync_api import Browser, Page, expect
 
 from pages.base_page import BasePage
 
 
 class LoginPage(BasePage):
     URL = 'https://practicetestautomation.com/practice-test-login/'
+    INVALID_USERNAME_ERROR = "Your username is invalid!"
+    INVALID_PASSWORD_ERROR = "Your password is invalid!"
 
     def __init__(self, page: Page):
         super().__init__(page)
         self.user_name = self.page.locator('#username')
         self.password = self.page.locator("#password")
         self.submit = self.page.locator("#submit")
+        self.successful_login = self.page.locator(".post-title")
+        self.login_failed = self.page.locator("#error")
 
     def login(self, user_name: str, password: str) -> None:
         self.user_name.fill(user_name)
         self.password.fill(password)
         self.submit.click()
+
+    def check_login_successful(self):
+        expect(self.successful_login).to_have_text("Logged In Successfully")
+
+    def check_login_invalid_user_name(self):
+        expect(self.login_failed).to_have_text(self.INVALID_USERNAME_ERROR)
+
+    def check_login_invalid_password(self):
+        expect(self.login_failed).to_have_text(self.INVALID_PASSWORD_ERROR)
--- tests/test_login.py
+++ tests/test_login.py
@@ -12,6 +12,29 @@ def login_credentials() -> tuple:
     return "student", "Password123"
 
 
+@pytest.fixture
+def invalid_user_name() -> tuple:
+    return "invalid_user", "Password123"
+
+
+@pytest.fixture
+def invalid_password() -> tuple:
+    return "student", "invalid_password"
+
+
 def test_basic_login_positive(login_page: LoginPage, login_credentials: tuple):
     login_page.visit()
     login_page.login(*login_credentials)
+    login_page.check_login_successful()
+
+
+def test_basic_login_invalid_username_negative(login_page: LoginPage, invalid_user_name: tuple):
+    login_page.visit()
+    login_page.login(*invalid_user_name)
+    login_page.check_login_invalid_user_name()
+
+
+def test_basic_login_invalid_password_negative(login_page: LoginPage, invalid_password: tuple):
+    login_page.visit()
+    login_page.login(*invalid_password)
+    login_page.check_login_invalid_password()
